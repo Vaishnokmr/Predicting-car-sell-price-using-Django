f4637a00701ecd61d12a95d6643c59cf96d9b72e
VAISHNO KUMAR <40652899+Vaishnokmr@users.noreply.github.com>
2021-11-09 01:43:14 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -137,3 +137,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #managing media
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
+
+#managing static root
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
