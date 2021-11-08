77e1a485a0952ae8c21d18e53d6d213fb9f7d165
VAISHNO KUMAR <40652899+Vaishnokmr@users.noreply.github.com>
2021-11-09 00:16:46 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w^(+r70p=q3u*!bv@a5b)o+k2xrqhtvy57q51#n$(=6zq8^#@$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','django-based-car-sell.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
