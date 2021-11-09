04541a6899eea38fec17b5ade3619ba2afd12734
VAISHNO KUMAR <40652899+Vaishnokmr@users.noreply.github.com>
2021-11-09 11:16:00 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,5 +145,6 @@ MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 
 
-
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
