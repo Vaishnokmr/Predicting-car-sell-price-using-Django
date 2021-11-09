07071c7a1494830424b8a636a80d98613499b2c5
VAISHNO KUMAR <40652899+Vaishnokmr@users.noreply.github.com>
2021-11-09 13:15:19 +0530

    Update urls.py

--- mac/urls.py
+++ mac/urls.py
@@ -25,6 +25,7 @@ from django.conf.urls import url
 urlpatterns = [
     path('shop/', include('shop.urls')),
     path('admin/', admin.site.urls),
+    url(r'^$', 'path.to.function'),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
