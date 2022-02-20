9b6be597067f08de83090edbc09af3cebf6499a2
VAISHNO KUMAR <40652899+Vaishnokmr@users.noreply.github.com>
2022-02-20 18:20:45 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 #managing static root
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
