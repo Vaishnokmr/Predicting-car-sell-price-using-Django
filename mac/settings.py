e53536aad6d1c6d9e11ebe6a6635c9e919faeb30
VAISHNO KUMAR <40652899+Vaishnokmr@users.noreply.github.com>
2022-02-21 00:02:30 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -90,7 +90,8 @@ DATABASES = {
 
 import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
