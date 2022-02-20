8c739f97b6504fc0be8dbae8da0b83ed54da01a0
VAISHNO KUMAR <40652899+Vaishnokmr@users.noreply.github.com>
2022-02-20 16:43:06 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.staticfiles.storage.StaticFilesStorage', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
