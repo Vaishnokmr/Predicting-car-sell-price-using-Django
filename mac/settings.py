92f0c522c16a4a3fb0d5f58473a0479104c2f64b
VAISHNO KUMAR <40652899+Vaishnokmr@users.noreply.github.com>
2022-02-20 17:09:56 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -135,6 +135,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   
+]
 
 
 # Default primary key field type
