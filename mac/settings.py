734e7be0bf05f575bf32cf1ca851e6e66ae764d0
VAISHNO KUMAR <40652899+Vaishnokmr@users.noreply.github.com>
2023-04-01 21:05:21 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 
 
@@ -150,6 +149,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+
 
