e2769199951b7b1909ad76be5cf2157d6dfb8bd3
VAISHNO KUMAR <40652899+Vaishnokmr@users.noreply.github.com>
2023-04-02 02:07:53 +0530

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='AboutUs'),
-    path('contact/', views.contact, name='ContactUs'),
     path('products/<int:myid>', views.productview, name='productview'),
     path('car/', views.car, name='SellCar'),
     path('car/result', views.result, name='result'),
